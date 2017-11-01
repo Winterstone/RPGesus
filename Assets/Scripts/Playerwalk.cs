f97f47201453c9c0a59ee719a5d28f0209bc982d
Kevin <25969875+Winterstone@users.noreply.github.com>
2017-11-01 15:55:14 -0700

    Initial Commit

---
+++ Assets/Scripts/Playerwalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerwalk : MonoBehaviour {
+    private float speed = 6;
+	private int health = 10;
+
+    void Update () {
+        float axisX = Input.GetAxis ("Horizontal");
+        float axisY = Input.GetAxis ("Vertical");
+
+        transform.Translate (new Vector3 (axisX, axisY) *Time.deltaTime * speed);
+		
+		if (health <= 0){
+			Destroy(gameObject);
+		}
+    }
+	
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "Enemy"){
+			//Destroy(other.gameObject);
+			health--;
+			Debug.Log ("Health: " + health);
+		}
+	}
+}
